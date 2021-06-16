a7a1ca616faaf012207139e67b3d914ce9023d03
rubeneggel72 <rubeneggel@gmail.com>
2021-06-16 18:50:13 -0300

    entrega proyecto

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const Sequelize = require('sequelize');
+require('dotenv').config({path: '../variable.env'})
+const db = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASS , {
+  host: process.env.DB_HOST,
+  dialect: 'mysql',
+  port: process.env.DB_PORT,
+  operatorsAliases: false,
+  define: {
+      timestamps: false
+  },
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+});
+
+module.exports = db;
---
+++ config/email.js
@@ -0,0 +1,18 @@
+// module.exports = {
+//     user: 'jermey23@ethereal.email',
+//     pass: 'ZYGgZGZS7SaWmNjKZd', 
+//     host: 'smtp.ethereal.email',
+//     port: '587'
+// }
+
+
+module.exports = {
+    service: 'gmail',
+    user: 'rubenalbertoeggel@gmail.com',
+    pass: 'auto4casa', 
+    host: 'smtp.gmail.email',
+    port: '587',
+    secure: false,
+requireTLS: true,
+}
+
