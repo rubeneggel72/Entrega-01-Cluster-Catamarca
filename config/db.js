8b1525b6fe666f13b3cad6902073626cdf4e66a2
rubeneggel72 <rubeneggel@gmail.com>
2021-06-16 21:16:55 -0300

    entrega

--- config/db.js
+++ config/db.js
@@ -1,11 +1,15 @@
 const Sequelize = require('sequelize');
 require('dotenv').config({path: '../variables.env'})
-const db = new Sequelize(process.env.DB_NOMBRE,process.env.DB_USER,process.env.DB_PASS , {
-  host: process.env.HOST,
-  dialect: 'mysql',
-  port: process.env.PORT,
-  operatorsAliases: false,
-  define: {
+const db = new Sequelize(
+  process.env.DB_NOMBRE,
+  process.env.DB_USER,
+  process.env.DB_PASS , 
+  {
+    host: process.env.DV_HOST,
+    dialect: 'mysql',
+    port: process.env.DB_PORT,
+    operatorsAliases: false,
+    define: {
       timestamps: false
   },
   pool: {
